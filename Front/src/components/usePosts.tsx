a84dafadb34b511dd531cb388c6ed0bb527560ea
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2024-08-07 23:09:15 +0300

    fix: delete button

--- Front/src/components/usePosts.tsx
+++ Front/src/components/usePosts.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 export interface Post {
   id: number;
@@ -10,17 +10,18 @@ export function usePosts(setIsLoggedIn: (value: boolean) => void) {
   const [posts, setPosts] = useState<Post[]>([]);
   const [searchValue, setSearchValue] = useState("");
 
-
   useEffect(() => {
     if (localStorage.getItem("TheToken")) {
       setIsLoggedIn(true);
     }
-   
 
     const fetchAllWords = async () => {
       try {
-        const response = await fetch("http://91.210.170.148/api/translator/get");
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const response = await fetch(
+          "http://91.210.170.148/api/translator/get"
+        );
+        if (!response.ok)
+          throw new Error(`HTTP error! status: ${response.status}`);
         const data = await response.json();
         const arrayOfWords = data.map((word: any) => ({
           id: word.id,
@@ -32,18 +33,18 @@ export function usePosts(setIsLoggedIn: (value: boolean) => void) {
         console.error("Failed to fetch words:", error);
       }
     };
-    
+
     fetchAllWords();
   }, [setIsLoggedIn]);
-  
+
   const handleDeletePost = (id: number) => {
     setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
   };
-  
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value.toLowerCase());
   };
-  
+
   const addNewPost = async (word: string, translation: string) => {
     try {
       const newWordData = await sendWords(word, translation);
@@ -53,7 +54,7 @@ export function usePosts(setIsLoggedIn: (value: boolean) => void) {
       console.error("Failed to add new word:", error);
     }
   };
-  
+
   const sendWords = async (en: string, ru: string) => {
     const response = await fetch("http://91.210.170.148/api/translator/write", {
       method: "PUT",
@@ -66,16 +67,20 @@ export function usePosts(setIsLoggedIn: (value: boolean) => void) {
     if (!response.ok) throw new Error("Ошибка при отправке данных на сервер");
     return await response.json();
   };
-  
+
   const arrange = () => {
     setPosts((prevPosts) => {
-      const sortedPosts = [...prevPosts].sort((a, b) => a.word.localeCompare(b.word));
+      const sortedPosts = [...prevPosts].sort((a, b) =>
+        a.word.localeCompare(b.word)
+      );
       return sortedPosts;
     });
   };
-  
+
   return {
-    posts: posts.filter((post) => post.word.toLowerCase().includes(searchValue)),
+    posts: posts.filter((post) =>
+      post.word.toLowerCase().includes(searchValue)
+    ),
     handleDeletePost,
     handleSearchChange,
     addNewPost,
